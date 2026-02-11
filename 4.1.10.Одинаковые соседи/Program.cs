ba7a649848007d3397d90b231ca07493e4c1223c
Vovan <vhotsanovich@yandex.ru>
2026-02-11 22:26:09 +0800

    4.1.10.Одинаковые соседи

---
+++ 4.1.10.Одинаковые соседи/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+internal class Program //4.1.10.Одинаковые соседи
+{
+    static void Main() 
+    {
+        string input = Console.ReadLine();
+        int counter = 0;
+        char previos_char = input[0];
+        for (int i = 1; i < input.Length; i++)
+        {
+            if (input[i] == previos_char) counter++;
+            previos_char = input[i];
+        }
+        Console.WriteLine(counter);
+    }
+}
