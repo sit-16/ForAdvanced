a38f8fa21d441e53f0d50c23ffb8d30259b80040
Vovan <vhotsanovich@yandex.ru>
2026-02-11 22:56:16 +0800

    4.2.Символы (сhar).06.Изменить регистр символа

---
+++ 4.2.Символы (сhar).06.Изменить регистр символа/Program.cs
@@ -0,0 +1,4 @@
+﻿// 4.2.Символы (сhar).06.Изменить регистр символа
+char ch = Convert.ToChar(Console.ReadLine());
+ch = char.IsUpper(ch) ? char.ToLower(ch) : char.ToUpper(ch);
+Console.WriteLine(ch);
