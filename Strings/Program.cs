1b51954c3f205f3e17859c5d11b9e48d40b80765
Vovan <vhotsanovich@yandex.ru>
2026-02-11 22:03:51 +0800

    Add project files.

---
+++ Strings/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Strings
+{
+    internal class Program //4.1.9.Strings
+    {
+        static void Main(string[] args)
+        {
+            int count_plus = 0;
+            int count_asterisk = 0;
+            string input = Console.ReadLine();
+            foreach (var ch in input)
+            { 
+                if (ch == '+') count_plus++;
+                else if (ch == '*') count_asterisk++;
+            }
+            Console.WriteLine($"Символ + встречается {count_plus} раз");
+            Console.WriteLine($"Символ * встречается {count_asterisk} раз");
+        }
+    }
+}
