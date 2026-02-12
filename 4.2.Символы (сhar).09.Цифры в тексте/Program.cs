d99a3f6ec4fe73a808304641b690d2b36032571a
Vovan <vhotsanovich@yandex.ru>
2026-02-12 23:07:16 +0800

    4.2.Символы (сhar).09.Цифры в тексте

---
+++ 4.2.Символы (сhar).09.Цифры в тексте/Program.cs
@@ -0,0 +1,8 @@
+﻿// 4.2.Символы (сhar).09.Цифры в тексте
+string input = Console.ReadLine();
+List<char> digits =    new List<char>();
+foreach (var ch in input)
+{
+    if (char.IsDigit(ch)) digits.Add(ch);
+}
+Console.WriteLine(string.Join(' ', digits));
