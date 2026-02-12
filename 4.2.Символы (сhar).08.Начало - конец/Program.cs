f93f039e3e176bcac44e3d25d1b7c77bb401c492
Vovan <vhotsanovich@yandex.ru>
2026-02-12 22:58:41 +0800

    4.2.Символы (сhar).08.Начало - конец

---
+++ 4.2.Символы (сhar).08.Начало - конец/Program.cs
@@ -0,0 +1,6 @@
+﻿// 4.2.Символы (сhar).08.Начало - конец
+string st1 = Console.ReadLine();
+string st2 = Console.ReadLine();
+if (char.ToLower(st1[0]) == char.ToLower(st2[^1]))
+    Console.WriteLine("YES");
+else Console.WriteLine("NO");
