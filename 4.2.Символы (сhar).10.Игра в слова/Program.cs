54ab4fecdcb3dcf576f0807d81a1355c3f3a90d3
Vovan <vhotsanovich@yandex.ru>
2026-02-12 23:42:16 +0800

    4.2.Символы (сhar).10.Игра в слова

---
+++ 4.2.Символы (сhar).10.Игра в слова/Program.cs
@@ -0,0 +1,19 @@
+﻿// 4.2.Символы (сhar).10.Игра в слова
+int n = Convert.ToInt32(Console.ReadLine());
+bool order = false; // Маша
+char curChar, prevChar;
+string input;
+prevChar = Console.ReadLine()[^1];
+for (int i = 1; i < n; i++)
+{
+    input = Console.ReadLine();
+    curChar = input[0];
+    if (char.ToLower(curChar) != prevChar) break;
+    prevChar = input[^1];
+    order = !order;
+}
+Console.WriteLine(order ? "Паша" : "Маша");
+
+
+
+
