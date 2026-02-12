cc6e565697477506facaa94e702401d7d3eec7fd
Vovan <vhotsanovich@yandex.ru>
2026-02-13 00:12:53 +0800

    4.2.Символы (сhar).11.Не та раскладка, Артур

---
+++ 4.2.Символы (сhar).11.Не та раскладка, Артур/Program.cs
@@ -0,0 +1,18 @@
+﻿// 4.2.Символы (сhar).11.Не та раскладка, Артур
+string englishAlphabet = "qwertyuiop[]asdfghjkl;'zxcvbnm,.";
+string russianAlphabet = "йцукенгшщзхъфывапролджэячсмитьбю";
+var dict = new Dictionary<char, char>();
+for (int i = 0; i < russianAlphabet.Length; i++)
+    dict.Add(russianAlphabet[i], englishAlphabet[i]);
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < n; i++)
+{
+    string input = Console.ReadLine();
+    string translate = string.Empty;
+    foreach (var ch in input)
+    {
+        if (dict.ContainsKey(ch)) translate += dict[ch];
+        else translate += ch;
+    }
+    Console.WriteLine(translate);
+}
