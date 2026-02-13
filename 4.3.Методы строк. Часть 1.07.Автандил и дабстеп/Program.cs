aa9df65dc325bb0f5b9f1fa7d45bdda68c66a0fe
Владимир Хоцанович <vhotsanovich@yandex.ru>
2026-02-13 22:47:35 +0800

    4.3.Методы строк. Часть 1.07.Автандил и дабстеп

---
+++ 4.3.Методы строк. Часть 1.07.Автандил и дабстеп/Program.cs
@@ -0,0 +1,4 @@
+﻿// 4.3.Методы строк. Часть 1.07.Автандил и дабстеп
+string input = Console.ReadLine();
+string[] output = input.Split("WUB", StringSplitOptions.RemoveEmptyEntries);
+Console.WriteLine(string.Join(' ', output));
