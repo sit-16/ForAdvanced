de373a828e45c342c1d298d7b56976b6fbb1b7e9
Владимир Хоцанович <vhotsanovich@yandex.ru>
2026-02-13 22:06:12 +0800

    4.3.Методы строк. Часть 1.04.Заглавные буквы

---
+++ 4.3.Методы строк. Часть 1.04.Заглавные буквы/Program.cs
@@ -0,0 +1,5 @@
+﻿// 4.3.Методы строк. Часть 1.04.Заглавные буквы
+string[] name = Console.ReadLine().Split();
+if (char.IsUpper(name[0][0]) && char.IsUpper(name[1][0]))
+    Console.WriteLine("YES");
+else Console.WriteLine("NO");
