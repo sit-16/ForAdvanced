d1f9fcd2c68ff1f679005d7de204b1fdb087de4d
Владимир Хоцанович <vhotsanovich@yandex.ru>
2026-02-14 21:53:46 +0800

    4.4 Методы строк. Часть 2.12.Расширение для браузера

---
+++ 4.4 Методы строк. Часть 2.11.Артур и строки/Program.cs
@@ -0,0 +1,26 @@
+﻿// 4.4 Методы строк. Часть 2.11.Артур и строки
+/*
+string stA = Console.ReadLine();
+string stB = Console.ReadLine();
+Console.WriteLine(string.Compare(stA.ToLower(), stB.ToLower()));
+*/
+
+string s = Console.ReadLine().ToLower();
+string t = Console.ReadLine().ToLower();
+
+int result = 0;
+
+for (int i = 0; i < s.Length; i++)
+{
+    if (s[i] < t[i])
+    {
+        result = -1;
+        break;
+    }
+    if (s[i] > t[i])
+    {
+        result = 1;
+        break;
+    }
+}
+Console.WriteLine(result);
---
+++ 4.4 Методы строк. Часть 2.12.Расширение для браузера/Program.cs
@@ -0,0 +1,12 @@
+﻿// 4.4 Методы строк. Часть 2.12.Расширение для браузера
+string input = Console.ReadLine();
+int low = 0, upp = 0;
+foreach (var ch in input)
+{
+    if (char.IsLower(ch)) low++;
+    else if (char.IsUpper(ch)) upp++;
+}
+
+if (upp > low) input = input.ToUpper();
+else input = input.ToLower();
+Console.WriteLine(input);
