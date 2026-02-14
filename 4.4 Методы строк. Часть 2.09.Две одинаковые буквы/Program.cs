4594b6391780220826a4b2d17bda774864b23b62
Владимир Хоцанович <vhotsanovich@yandex.ru>
2026-02-14 19:30:05 +0800

    4.4 Методы строк. Часть 2.09.Две одинаковые буквы

---
+++ 4.4 Методы строк. Часть 2.09.Две одинаковые буквы/Program.cs
@@ -0,0 +1,13 @@
+﻿// 4.4 Методы строк. Часть 2.09.Две одинаковые буквы
+string st = Console.ReadLine();
+char ch = st[0];
+while(true)
+{
+    st = st.Substring(1);
+    if (st.Contains(ch))
+    {
+        Console.WriteLine(ch);
+        break;
+    }
+    ch = st[0];
+}
