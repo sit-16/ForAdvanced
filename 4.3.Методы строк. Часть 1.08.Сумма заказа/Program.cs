7970dc23560c12995219ae3a0731ade7c7b57216
Владимир Хоцанович <vhotsanovich@yandex.ru>
2026-02-14 08:44:49 +0800

    4.3.Методы строк. Часть 1.08.Сумма заказа

---
+++ 4.3.Методы строк. Часть 1.08.Сумма заказа/Program.cs
@@ -0,0 +1,11 @@
+﻿// 4.3.Методы строк. Часть 1.08.Сумма заказа
+using System.Globalization;
+
+int n  = Convert.ToInt32(Console.ReadLine());
+double summ = 0;
+for (int i = 0; i < n; i++)
+{ 
+    string[] input = Console.ReadLine().Split('#');
+    summ += Convert.ToDouble(input[2],CultureInfo.CreateSpecificCulture("en-EN"));
+}
+Console.WriteLine(summ.ToString("0.00"));
