969e95779238c8778849b5da6148c38107f88bf7
Владимир Хоцанович <vhotsanovich@yandex.ru>
2026-02-14 13:03:07 +0800

    4.3.Методы строк. Часть 1.10.Азбука Арторзе

---
+++ 4.3.Методы строк. Часть 1.10.Азбука Арторзе/Program.cs
@@ -0,0 +1,6 @@
+﻿// 4.3.Методы строк. Часть 1.10.Азбука Арторзе
+string input = Console.ReadLine();
+string result = input.Replace("--", "2");
+result = result.Replace("-.", "1");
+result = result.Replace(".", "0");
+Console.WriteLine(result);
