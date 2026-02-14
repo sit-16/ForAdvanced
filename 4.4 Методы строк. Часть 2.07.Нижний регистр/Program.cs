c61020c8f533f42635b886886ac15fcbc8ca6e63
Владимир Хоцанович <vhotsanovich@yandex.ru>
2026-02-14 13:38:36 +0800

    4.4 Методы строк. Часть 2.07.Нижний регистр

---
+++ 4.4 Методы строк. Часть 2.07.Нижний регистр/Program.cs
@@ -0,0 +1,11 @@
+﻿// 4.4 Методы строк. Часть 2.07.Нижний регистр
+string input = Console.ReadLine();
+int counter = 0;
+for (int i = 0; i < input.Length; i++)
+{
+    string test = input.Substring(i, 1);
+    string low = test.ToLower();
+    string upp = test.ToUpper();
+    if (low != upp && test == low) counter++;
+}
+Console.WriteLine(counter);
