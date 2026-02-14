6b67ee563388ae946bce0229d5bb6e593e5b7eb3
Владимир Хоцанович <vhotsanovich@yandex.ru>
2026-02-14 11:05:28 +0800

    4.3.Методы строк. Часть 1.09.Самый длинный префикс

--- 4.3.Методы строк. Часть 1.05.k-ый символ/Program.cs
+++ 4.3.Методы строк. Часть 1.05.k-ый символ/Program.cs
@@ -1,4 +1,6 @@
 ﻿// 4.3.Методы строк. Часть 1.05.k-ый символ
-string st = Console.ReadLine();
-int n = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(st.Remove(--n, 1));
+string st = Console.ReadLine(); // 0123
+int n = Convert.ToInt32(Console.ReadLine()); // 1
+Console.WriteLine(st.Remove(--n, 1)); // 123
+//Console.WriteLine(st.Remove(n, 1)); // 023 
+//Console.WriteLine(st.Remove(n--, 1)); // 023
---
+++ 4.3.Методы строк. Часть 1.09.Самый длинный префикс/Program.cs
@@ -0,0 +1,20 @@
+﻿// 4.3.Методы строк. Часть 1.09.Самый длинный префикс
+
+int n = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[n];
+for (int i = 0; i < n; i++) array[i] = Console.ReadLine();
+string pref = string.Empty;
+for (int i = 0; i < array[0].Length; i++)
+{
+    char ch = array[0][i];
+    bool flag = true;
+    for (int j = 1; j < n; j++)
+    {
+        if (i >= array[j].Length) { flag = false; break; }
+        if (array[j] == string.Empty) { flag = false; break; }
+        if (array[j].Substring(i, 1)[0] != ch) { flag = false; break; }
+    }
+    if (flag) pref += ch;
+    else break;
+}
+Console.WriteLine(pref == string.Empty ? "НЕТ" : pref);
