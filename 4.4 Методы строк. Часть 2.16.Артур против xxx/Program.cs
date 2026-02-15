6e54061b29f7f22467578b936b3edc6722ddc614
Владимир Хоцанович <vhotsanovich@yandex.ru>
2026-02-15 20:59:36 +0800

    4.4 Методы строк. Часть 2.16.Артур против xxx

---
+++ 4.4 Методы строк. Часть 2.16.Артур против xxx/Program.cs
@@ -0,0 +1,31 @@
+﻿// 4.4 Методы строк. Часть 2.16.Артур против xxx
+string input = Console.ReadLine();
+string result = input;
+while (result.Contains("xxx"))
+{
+    result = result.Replace("xxx","xx");
+    Console.WriteLine(result);
+}
+Console.WriteLine(input.Length - result.Length);
+
+
+
+
+
+
+/*
+int collect = 0;
+int counter = 0;
+foreach (var ch in input)
+{
+    if (ch == 'x') collect++;
+    else collect = 0;
+    if (collect == 3)
+    {
+        counter++;
+        collect--;
+        continue; 
+    }
+}
+Console.WriteLine(counter);
+*/
