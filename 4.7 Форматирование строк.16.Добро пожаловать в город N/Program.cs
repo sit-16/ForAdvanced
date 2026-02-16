bbe144bb93011b0fc99c0e1212bb109957c33b7d
Владимир Хоцанович <vhotsanovich@yandex.ru>
2026-02-16 21:28:18 +0800

    4.7 Форматирование строк.16.Добро пожаловать в город N

---
+++ 4.7 Форматирование строк.16.Добро пожаловать в город N/Program.cs
@@ -0,0 +1,7 @@
+﻿// 4.7 Форматирование строк.16.Добро пожаловать в город N
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < n; i++)
+{
+    string[] input = Console.ReadLine().Split();
+    Console.WriteLine("Добрый день, {0}! Рады приветствовать вас в городе {1}!", input[0], input[1]);
+}
