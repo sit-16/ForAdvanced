0f562b7f48e29998bac5fb9fbd3fc734a9334726
Владимир Хоцанович <vhotsanovich@yandex.ru>
2026-02-16 20:37:12 +0800

    4.7 Форматирование строк.15.Кастомный разделитель

---
+++ 4.7 Форматирование строк.15.Кастомный разделитель/Program.cs
@@ -0,0 +1,6 @@
+﻿// 4.7 Форматирование строк.15.Кастомный разделитель
+string sep = Console.ReadLine();
+string a = Console.ReadLine();
+string b = Console.ReadLine();
+string c = Console.ReadLine();
+Console.WriteLine("{1}{0}{2}{0}{3}", sep, a, b, c);
