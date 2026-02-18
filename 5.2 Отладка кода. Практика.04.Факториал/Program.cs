c816249036a7faeaf57d8273b3b80766be91896c
Владимир Хоцанович <vhotsanovich@yandex.ru>
2026-02-18 22:16:49 +0800

    5.2 Отладка кода. Практика.04.Факториал

---
+++ 5.2 Отладка кода. Практика.04.Факториал/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _5._2_Отладка_кода._Практика._04.Факториал
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int a = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                a = a * i;
+            }
+
+            Console.WriteLine(a);
+        }
+    }
+}
