3e2318dfed442bbc8bf143c06a7a6d406f872fb1
Владимир Хоцанович <vhotsanovich@yandex.ru>
2026-02-18 22:47:34 +0800

    5.2 Отладка кода. Практика.07.Старшая цифра

--- 5.2 Отладка кода. Практика.07.Старшая цифра/Program.cs
+++ 5.2 Отладка кода. Практика.07.Старшая цифра/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             int n = Convert.ToInt32(Console.ReadLine());
-            while (n > 10)
+            while (n >= 10)
             {
                 n = n / 10;
             }
