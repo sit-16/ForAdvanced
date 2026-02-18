0909f6e5b14c6ae64c880c9732c8acaf58d57c52
Владимир Хоцанович <vhotsanovich@yandex.ru>
2026-02-18 22:10:04 +0800

    5.2 Отладка кода. Практика.03.Перебор чисел с условием 2

---
+++ 5.2 Отладка кода. Практика.03.Перебор чисел с условием 2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _5._2_Отладка_кода._Практика._03.Перебор_чисел_с_условием_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = 0;
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    a = a + i;
+                }
+            }
+
+            Console.WriteLine(a);
+        }
+    }
+}
