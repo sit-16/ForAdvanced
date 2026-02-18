dbe601a4b5e830378052b4de535642980e008821
Владимир Хоцанович <vhotsanovich@yandex.ru>
2026-02-18 22:34:57 +0800

    5.2 Отладка кода. Практика.06.Произведение цифр числа

---
+++ 5.2 Отладка кода. Практика.06.Произведение цифр числа/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _5._2_Отладка_кода._Практика._06.Произведение_цифр_числа
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int product = 1;
+            while (n > 0)
+            {
+                int digit = n % 10;
+                product = product * digit;
+                n = n / 10;
+            }
+
+            Console.WriteLine(product);
+        }
+    }
+}
