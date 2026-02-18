83179d2addf063f9f5771ef430310cd02cc8fbbc
Владимир Хоцанович <vhotsanovich@yandex.ru>
2026-02-18 22:39:00 +0800

    5.2 Отладка кода. Практика.07.Старшая цифра

---
+++ 5.2 Отладка кода. Практика.07.Старшая цифра/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _5._2_Отладка_кода._Практика._07.Старшая_цифра
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            while (n > 10)
+            {
+                n = n / 10;
+            }
+            Console.WriteLine(n);
+        }
+    }
+}
