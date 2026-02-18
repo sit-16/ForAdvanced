58277cfac98b8b4ab46efe56036c61d14a25fdc8
Владимир Хоцанович <vhotsanovich@yandex.ru>
2026-02-18 22:23:48 +0800

    5.2 Отладка кода. Практика.05.Cумма чётных чисел

---
+++ 5.2 Отладка кода. Практика.05.Cумма чётных чисел/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _5._2_Отладка_кода._Практика._05.Cумма_чётных_чисел
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int s = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                int n = Convert.ToInt32(Console.ReadLine());
+                if (n % 2 == 0)
+                {
+                    s = s + n;
+                }
+            }
+
+            Console.WriteLine(s);
+        }
+    }
+}
