5576493f59945a2e1a8129f4befb77f5bec00a7e
Владимир Хоцанович <vhotsanovich@yandex.ru>
2026-02-19 10:46:06 +0800

    5.2 Отладка кода. Практика.08.Кол-во и пр-ие неотр-х чисел

---
+++ 5.2 Отладка кода. Практика.08.Кол-во и пр-ие неотр-х чисел/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _5._2_Отладка_кода._Практика._08.Кол_во_и_пр_ие_неотр_х_чисел
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            int p = 1;
+            for (int i = 0; i < 10; i++)
+            {
+                int x = Convert.ToInt32(Console.ReadLine());
+                if (x >= 0)
+                {
+                    p = p * x;
+                    count = count + 1;
+                }
+            }
+            if (count > 0)
+            {
+                Console.WriteLine(count);
+                Console.WriteLine(p);
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
