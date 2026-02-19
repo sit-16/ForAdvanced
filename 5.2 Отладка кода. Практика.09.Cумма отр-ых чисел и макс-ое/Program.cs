6ca48b180cfc2cc25a86e62e8808c34f83aac053
Владимир Хоцанович <vhotsanovich@yandex.ru>
2026-02-19 10:55:11 +0800

    5.2 Отладка кода. Практика.09.Cумма отр-ых чисел и макс-ое

---
+++ 5.2 Отладка кода. Практика.09.Cумма отр-ых чисел и макс-ое/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _5._2_Отладка_кода._Практика._09.Cумма_отр_ых_чисел_и_макс_ое
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int mx = 0;
+            int s = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                int x = Convert.ToInt32(Console.ReadLine());
+                if (x < 0)
+                {
+                    s += x;
+                    if (x > mx || mx == 0)
+                    {
+                        mx = x;
+                    }
+                }
+            }
+            if (mx == 0) Console.WriteLine("NO");
+            else
+            {
+                Console.WriteLine(s);
+                Console.WriteLine(mx);
+            }
+        }
+    }
+}
