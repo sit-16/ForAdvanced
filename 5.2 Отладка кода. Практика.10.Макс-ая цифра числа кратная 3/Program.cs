6b61a021fad786cc1c61058f99971d2b755b9af3
Владимир Хоцанович <vhotsanovich@yandex.ru>
2026-02-19 11:02:41 +0800

    5.2 Отладка кода. Практика.10.Макс-ая цифра числа кратная 3

---
+++ 5.2 Отладка кода. Практика.10.Макс-ая цифра числа кратная 3/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _5._2_Отладка_кода._Практика._10.Макс_ая_цифра_числа_кратная_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int maxDigit = -1;
+            while (n > 0)
+            {
+                int digit = n % 10;
+                if (digit % 3 == 0)
+                {
+                    if (digit > maxDigit)
+                    {
+                        maxDigit = digit;
+                    }
+                }
+                n = n / 10;
+            }
+            if (maxDigit == -1)
+            {
+                Console.WriteLine("NO");
+            }
+            else
+            {
+                Console.WriteLine(maxDigit);
+            }
+        }
+    }
+}
