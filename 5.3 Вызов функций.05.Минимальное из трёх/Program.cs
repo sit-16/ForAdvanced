6ed22f554a4b7446463384e1f4f70c599965f8f4
Владимир Хоцанович <vhotsanovich@yandex.ru>
2026-02-19 20:30:55 +0800

    5.3 Вызов функций.05.Минимальное из трёх

---
+++ 5.3 Вызов функций.05.Минимальное из трёх/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _5._3_Вызов_функций._05.Минимальное_из_трёх
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            double a = ReadNumber();
+            double b = ReadNumber();
+            double c = ReadNumber();
+
+            double min = MinOfThree(a, b, c);
+
+            Console.WriteLine(min);
+        }
+
+        static double ReadNumber()
+        {
+            return double.Parse(Console.ReadLine());
+        }
+
+        static double Min(double a, double b)
+        {
+            return a < b ? a : b;
+        }
+
+        static double MinOfThree(double a, double b, double c)
+        {
+            double minAB = Min(a, b);
+            return Min(minAB, c);
+        }
+    }
+}
