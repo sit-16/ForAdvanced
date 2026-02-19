f635dcbcc8dc290c93072378d051d8d5b84be5f7
Владимир Хоцанович <vhotsanovich@yandex.ru>
2026-02-19 20:40:08 +0800

    5.3 Вызов функций.06.Поиск символа

---
+++ 5.3 Вызов функций.06.Поиск символа/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _5._3_Вызов_функций._06.Поиск_символа
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            string firstSentence = Console.ReadLine();
+            string secondSentence = Console.ReadLine();
+            Console.Write(CountLetterC(firstSentence) + CountLetterC(secondSentence));
+        }
+
+        static int CountLetterC(string sentence)
+        {
+            int count = 0;
+
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if (sentence[i] == 'с')
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
