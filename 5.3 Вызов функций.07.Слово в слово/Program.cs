c9b77583ae5774c7a31730ba4fe8c0fd51b41f41
Владимир Хоцанович <vhotsanovich@yandex.ru>
2026-02-19 20:50:28 +0800

    5.3 Вызов функций.07.Слово в слово

---
+++ 5.3 Вызов функций.07.Слово в слово/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _5._3_Вызов_функций._07.Слово_в_слово
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string sentence = Console.ReadLine();
+            string word = Console.ReadLine();
+
+            bool result = ContainsFullWord(sentence, word);
+            Console.WriteLine(result);
+        }
+
+        static bool ContainsFullWord(string sentence, string word)
+        {
+            string[] words = sentence.Split(" ");
+
+            foreach (string w in words)
+            {
+                if (w == word)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
