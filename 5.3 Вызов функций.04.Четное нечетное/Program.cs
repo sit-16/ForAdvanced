2d5e0b3ced6a3ffccb35bd54550dd0ab13dab781
Владимир Хоцанович <vhotsanovich@yandex.ru>
2026-02-19 20:24:27 +0800

    5.3 Вызов функций.04.Четное нечетное

---
+++ 5.3 Вызов функций.04.Четное нечетное/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _5._3_Вызов_функций._04.Четное_нечетное
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int a = ReadNumber();
+            int b = ReadNumber();
+
+            int sum = Sum(a, b);
+
+            Console.WriteLine("Сума: " + sum);
+
+            if (IsEven(sum))
+            {
+                Console.WriteLine("Сумма четная ");
+            }
+            else
+            {
+                Console.WriteLine("Сумма нечетная ");
+            }
+        }
+
+        static int ReadNumber()
+        {
+            return int.Parse(Console.ReadLine());
+        }
+
+        static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        static bool IsEven(int x)
+        {
+            return x % 2 == 0;
+        }
+    }
+}
