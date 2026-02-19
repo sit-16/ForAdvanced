7be834540d75946fe3ab3188bc09024316727189
Владимир Хоцанович <vhotsanovich@yandex.ru>
2026-02-19 20:58:27 +0800

    5.3 Вызов функций.08.Слитно

---
+++ 5.3 Вызов функций.08.Слитно/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _5._3_Вызов_функций._08.Слитно
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+
+            string result = RemoveSpaces(input);
+
+            Console.WriteLine(result);
+        }
+
+        static string RemoveSpaces(string text)
+        {
+            string result = "";
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] != ' ')
+                {
+                    result += text[i];
+                }
+            }
+
+            return result;
+        }
+    }
+}
