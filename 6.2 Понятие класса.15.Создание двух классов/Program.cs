b3802e014d5da36a85aa9f844168a1890d0f46a3
Владимир Хоцанович <vhotsanovich@yandex.ru>
2026-02-20 23:54:53 +0800

    6.2 Понятие класса.15.Создание двух классов

---
+++ 6.2 Понятие класса.15.Создание двух классов/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _6._2_Понятие_класса._15.Создание_двух_классов
+{
+    public class GeoLocation
+    {
+        public double Latitude;
+        public double Longitude;
+    }
+
+    public class City
+    {
+        public string Name;
+        public GeoLocation Location;
+    }
+
+        
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            City city = new City();
+
+            city.Name = "Vladikavkaz";
+
+            GeoLocation location = new GeoLocation();
+
+            location.Latitude = 43.0348;
+            location.Longitude = 44.6798;
+
+            city.Location = location;
+
+            Console.WriteLine($"I love {city.Name} located at ({city.Location.Longitude}, {city.Location.Latitude})");
+        }
+    }
+}
