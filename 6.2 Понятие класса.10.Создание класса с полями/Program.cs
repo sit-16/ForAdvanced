86e8c57faed5c5a149adf9488bccf6dc5e7677c5
Владимир Хоцанович <vhotsanovich@yandex.ru>
2026-02-20 23:39:45 +0800

    6.2 Понятие класса.10.Создание класса с полями

---
+++ 6.2 Понятие класса.10.Создание класса с полями/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _6._2_Понятие_класса._10.Создание_класса_с_полями
+{
+    public class School
+    {
+        public string Name = "Неизвестно";
+        public int Number = -1;
+    }
+
+    public class Student
+    {
+        public string FirstName = "Неизвестное имя";
+        public string LastName = "Неизвестная фамилия";
+        public int Age = -1;
+        public double Rating = 0.1;
+    }
+
+    internal class Program
+    {     
+        static void Main(string[] args)
+        {
+            var test = new School();
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
