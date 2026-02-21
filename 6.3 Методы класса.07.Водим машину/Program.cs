18ee7171cb036b6204528c338cb3154102e15b3f
Владимир Хоцанович <vhotsanovich@yandex.ru>
2026-02-21 15:20:51 +0800

    6.3 Методы класса.07.08.Машина.Зебра

---
+++ 6.3 Методы класса.07.Водим машину/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Runtime.ConstrainedExecution;
+
+namespace _6._3_Методы_класса._07.Водим_машину
+{
+    public class Car
+    {
+        public void Drive()
+        {
+            Console.WriteLine("Вын-вын!");
+        }
+    }
+
+    public class Zebra
+    {
+        string[] color = { "Полоска белая", "Полоска черная" };
+        bool isBlack = false;
+        public void GetStripe()
+        {
+            Console.WriteLine(color[isBlack ? 1 : 0]);
+            isBlack = !isBlack;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Car car = new Car();
+            //car.Drive();
+            Zebra zebra1 = new Zebra();
+            zebra1.GetStripe();
+            zebra1.GetStripe();
+            zebra1.GetStripe();
+            zebra1.GetStripe();
+            Console.WriteLine();
+            Zebra zebra2 = new Zebra();
+            zebra2.GetStripe();
+            zebra2.GetStripe();
+        }
+    }
+}
