a0f3b3da8ff279976e310489d21e3b0196042ad6
Владимир Хоцанович <vhotsanovich@yandex.ru>
2026-02-21 17:45:46 +0800

    6.3 Методы класса.09-11.Счётчик

--- 6.3 Методы класса.07.Водим машину/Program.cs
+++ 6.3 Методы класса.07.Водим машину/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.ConstrainedExecution;
+﻿using System.Diagnostics.Metrics;
+using System.Runtime.ConstrainedExecution;
 
 namespace _6._3_Методы_класса._07.Водим_машину
 {
@@ -21,21 +22,62 @@ namespace _6._3_Методы_класса._07.Водим_машину
         }
     }
 
+    public class Counter
+    {
+        int count = 0;
+        public void Increment()
+        {
+            count++;
+        }
+        public void Reset()
+        {
+            count = 0;
+        }
+        public void StartFrom(int n)
+        {
+            count = n;
+        }
+        public void Show()
+        {
+            Console.WriteLine("Текущее значение счетчика = {0}", count);
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
             //Car car = new Car();
             //car.Drive();
-            Zebra zebra1 = new Zebra();
-            zebra1.GetStripe();
-            zebra1.GetStripe();
-            zebra1.GetStripe();
-            zebra1.GetStripe();
-            Console.WriteLine();
-            Zebra zebra2 = new Zebra();
-            zebra2.GetStripe();
-            zebra2.GetStripe();
+
+            //Zebra zebra1 = new Zebra();
+            //zebra1.GetStripe();
+            //zebra1.GetStripe();
+            //zebra1.GetStripe();
+            //zebra1.GetStripe();
+            //Console.WriteLine();
+            //Zebra zebra2 = new Zebra();
+            //zebra2.GetStripe();
+            //zebra2.GetStripe();
+
+            Counter c1 = new Counter();
+            c1.Show();
+
+            c1.Increment();
+            c1.Show();
+
+            c1.Increment();
+            c1.Increment();
+            c1.Show();
+
+            c1.Increment();
+            c1.Show();
+            c1.Show();
+
+            c1.Increment();
+            c1.Increment();
+            c1.Increment();
+            c1.Show();
         }
     }
 }
