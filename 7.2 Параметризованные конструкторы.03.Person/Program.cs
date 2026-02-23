c66ad07e462678f3c4560f0e72f92c2aa59e2a81
Владимир Хоцанович <vhotsanovich@yandex.ru>
2026-02-23 12:37:57 +0800

    7.2 Параметризованные конструкторы.03.Person

---
+++ 7.2 Параметризованные конструкторы.03.Person/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _7._2_Параметризованные_конструкторы._03.Person
+{
+    internal class Program
+    {
+        public class Person
+        {
+            public string FirstName;
+            public string LastName;
+            public int Age;
+
+            public Person(string sFirstName, string sLastName, int iAge)
+            {
+                FirstName = sFirstName;
+                LastName = sLastName;
+                Age = iAge;
+            }
+
+            public string GetFullName()
+                { return $"{LastName} {FirstName}"; }
+            public bool IsAdult()
+                { return Age>=18; }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Person person = new Person("Иосиф", "Дзеранов", 26);
+            Console.WriteLine(person.GetFullName());
+            Console.WriteLine(person.IsAdult());
+        }
+    }
+}
