5de608db26b6281cd08a3d1d2a60d69bbf56033a
Владимир Хоцанович <vhotsanovich@yandex.ru>
2026-02-23 22:54:53 +0800

    7.2 Параметризованные конструкторы.11.Множество конструкторов

---
+++ 7.2 Параметризованные конструкторы.11.Множество конструкторов/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _7._2_Параметризованные_конструкторы._11.Множество_конструкторов
+{
+    public class MyClass
+    {
+        public MyClass(string st)
+        {}
+        public MyClass(string st, int i)
+        { }
+        public MyClass(string st, int i, bool b)
+        { }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass m1 = new MyClass("test");
+            MyClass m2 = new MyClass("test2", 4);
+            MyClass m3 = new MyClass("test3", 4, false);
+        }
+    }
+}
