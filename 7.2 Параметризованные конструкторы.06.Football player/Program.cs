39bfe107a58c8d86c52dd8c9e2c9dc0abecba7a5
Владимир Хоцанович <vhotsanovich@yandex.ru>
2026-02-23 21:06:54 +0800

    7.2 Параметризованные конструкторы.06.Football player

---
+++ 7.2 Параметризованные конструкторы.06.Football player/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _7._2_Параметризованные_конструкторы._06.Football_player
+{
+    public class FootballPlayer
+    {
+        public string Name;
+        public string Surname;
+        public int Goals;
+        public int Assists;
+        public FootballPlayer(string name, string surname)
+        {
+            Name = name;
+            Surname = surname;
+            Goals = 0;
+            Assists = 0;
+        }
+        public void AddScore(int value)
+        {
+            Goals+= value;
+        }
+        public void AddAssists(int value)
+        {
+            Assists+= value;
+        }
+        public void ShowStatistics()
+        {
+            Console.WriteLine($"{Surname} {Name} - голы: {Goals}, передачи: {Assists}");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FootballPlayer player1 = new FootballPlayer("Cristiano", "Ronaldo");
+            player1.AddScore(711);
+            player1.AddAssists(169);
+            player1.ShowStatistics();
+
+            FootballPlayer player2 = new FootballPlayer("Lionel", "Messi");
+            player2.AddScore(682);
+            player2.AddAssists(240);
+            player2.ShowStatistics();
+        }
+    }
+}
