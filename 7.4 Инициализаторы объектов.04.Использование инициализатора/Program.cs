af03a88934d7bfcb43f47e6b4315705d58d2cc05
Владимир Хоцанович <vhotsanovich@yandex.ru>
2026-02-24 14:51:57 +0800

    7.4 Инициализаторы объектов.04.Использование инициализатора

---
+++ 7.4 Инициализаторы объектов.04.Использование инициализатора/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _7._4_Инициализаторы_объектов._04.Использование_инициализатора
+{
+    public class Phone
+    {
+        public string Name; // название
+        public int Price; // цена в рублях
+        public int Memory; // объем памяти в ГБ
+        public bool IsAvailable; // доступна или нет
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Phone myPhone = new Phone()
+            {
+                Name = "iPhone 12 mini", // название
+                Price = 70000, // цена в рублях
+                Memory = 128, // объем памяти в ГБ
+                IsAvailable = true // доступна или нет 
+            };
+        }
+    }
+}
