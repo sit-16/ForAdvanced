fa903fe120a9cf755e0738e62d7bcaadcb728b64
Владимир Хоцанович <vhotsanovich@yandex.ru>
2026-02-24 15:54:27 +0800

    8.1 Моделирование работы школы - выполнено задание

--- 8.1 Моделирование работы школы/Program.cs
+++ 8.1 Моделирование работы школы/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace _8._1_Моделирование_работы_школы
 {
@@ -7,23 +8,26 @@ namespace _8._1_Моделирование_работы_школы
     {
         public string FirstName;
         public string LastName;
-        public Student(string firstName, string lastName)
+        public int Age;
+        public Student(string firstName, string lastName, int age)
         {
             FirstName = firstName;
             LastName = lastName;
+            Age = age;
         }
     }
 
     public class School
     {
         public string Name;
-        public List<Student> Students;
+        public Dictionary<int, Student> Students;
 
         public School(string name)
         {
             Name = name;
-            Students = new List<Student>();
+            Students = new Dictionary<int, Student>();
         }
+
 
         public void PrintStudents()
         {
@@ -33,23 +37,50 @@ namespace _8._1_Моделирование_работы_школы
             }
             else
             {
-                Console.WriteLine("{0, -10} {1, -10}", "Имя", "Фамилия");
+                Console.WriteLine("{0, -10} {1, -10} {2, -10} {3, -10}", "#", "Имя", "Фамилия", "Возраст");
                 for (int i = 0; i < Students.Count; i++)
                 {
-                    Console.WriteLine("{0, -10} {1, -10}", Students[i].FirstName, Students[i].LastName);
+                    Console.WriteLine("{0, -10} {1, -10} {2, -10} {3, -10}", i, Students[i].FirstName, Students[i].LastName, Students[i].Age);
                 }
             }
         }
 
+
         public void AddNewStudent(Student student)
         {
-            Students.Add(student);
+            int nextN = Students.Count;
+            Students.Add(nextN, student);
             Console.WriteLine($"Студент {student.FirstName} успешно добавлен в школу {Name}.");
+        }
+
+
+        public bool RemoveStudent(string input)
+        {
+            if (!int.TryParse(input, out int num)) return false;
+            if (!this.Students.ContainsKey(num)) return false;
+            string lastName = Students[num].LastName;
+            Students.Remove(num);
+            int i = 0;
+            var tempStudents = new Dictionary<int, Student>();
+            foreach (var st in this.Students)
+            {
+                tempStudents.Add(i, st.Value);
+                i++;
+            }
+            this.Students = tempStudents;
+            Console.WriteLine($"Студент {lastName} успешно отчислен из школы {this.Name}.");
+            return true;
         }
     }
 
     class Program
     {
+        static bool AskUserDa()
+        {
+            string userAnswer = Console.ReadLine().ToLower();
+            return userAnswer == "да";
+        }
+
         static void Main()
         {
             Console.WriteLine("Здравствуйте! Введите название вашей школы");
@@ -60,23 +91,32 @@ namespace _8._1_Моделирование_работы_школы
             while (true)
             {
                 Console.WriteLine($"Хотите посмотреть список учеников школы {school.Name}? Введите да или нет. ");
-                string userAnswer = Console.ReadLine();
-                if (userAnswer == "да")
+                if (AskUserDa())
                 {
                     school.PrintStudents();
                 }
 
                 Console.WriteLine($"Хотите добавить нового ученика в школу {school.Name}? Введите да или нет. ");
-                userAnswer = Console.ReadLine();
-                if (userAnswer == "да")
+                if (AskUserDa())
                 {
                     Console.WriteLine($"Введите имя ученика");
                     string firstName = Console.ReadLine();
                     Console.WriteLine($"Введите фамилию ученика");
                     string lastName = Console.ReadLine();
+                    Console.WriteLine($"Введите возраст ученика");
+                    int age = Convert.ToInt32(Console.ReadLine());
 
-                    Student student = new Student(firstName, lastName);
+                    Student student = new Student(firstName, lastName, age);
                     school.AddNewStudent(student);
+                }
+                Console.WriteLine($"Хотите исключить ученика из школы {school.Name}? Введите да или нет. ");
+                if (AskUserDa())
+                {
+                    Console.WriteLine($"Введите номер ученика");
+                    while (!school.RemoveStudent(Console.ReadLine()))
+                    {
+                        Console.WriteLine($"Введите номер ученика");
+                    }
                 }
             }
         }
