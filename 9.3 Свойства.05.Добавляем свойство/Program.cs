0accb9f234f5d44f42265c793775773cd043e955
Владимир Хоцанович <vhotsanovich@yandex.ru>
2026-02-27 22:23:52 +0800

    9.3 Свойства.06.Защищаем возраст ученика 3

---
+++ 9.3 Свойства.05.Добавляем свойство/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _9._3_Свойства._05.Добавляем_свойство
+{
+    internal class Program
+    {
+        public class MyClass
+        {
+            private int field;
+            public int Property
+            {
+                get { return field; }
+                set { field = value; }
+            }
+        }
+        static void Main(string[] args)
+        {
+            MyClass instance = new MyClass();
+            instance.Property = 14;
+            Console.WriteLine(instance.Property);
+        }
+    }
+}
---
+++ 9.3 Свойства.06.Защищаем возраст ученика 3/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace sit16.ProtectStudentAge3
+{
+    public class Student
+    {
+        private string name;
+        private int age;
+
+        public string Name
+        {
+            // геттер
+            get
+            {
+                return name;
+            }
+            // Сеттер
+            set
+            {
+                if (value.Length <= 1)
+                {
+                    throw new Exception("Имя должно состоять как минимум из 2 букв.");
+                }
+
+                foreach (var c in value)
+                {
+                    if (!char.IsLetter(c))
+                    {
+                        throw new Exception("Имя должно состоять только букв.");
+                    }
+                }
+
+                name = value;
+            }
+        }
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value < 16) throw new Exception(@"//ошибка, так как меньше предыдущего возраста");
+                if (value < age) throw new Exception(@"//ошибка, так как некорректный возраст");
+                age = value;
+            }
+        }
+
+        public Student(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student("Иосиф", 16);
+            Console.WriteLine(student1.Age);
+
+            student1.Age = 25;
+            Console.WriteLine(student1.Age);
+
+            student1.Age = 20;
+            student1.Age = 15;
+        }
+    }
+}
