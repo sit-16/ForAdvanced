b4392657c0878ceecc3a64014dd569a03908fa6e
Владимир Хоцанович <vhotsanovich@yandex.ru>
2026-03-01 01:27:32 +0800

    9.4 readonly поля.08.Дробь

---
+++ 9.4 readonly поля.08.Дробь/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.ObjectModel;
+using System.Globalization;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace _9._4_readonly_поля._08.Дробь
+{
+    public class Fraction
+    {
+        public readonly int Numerator; // числитель
+        public readonly int Denominator; // знаменатель
+        public readonly double Value; // значение дроби
+
+        public Fraction(int num, int den)
+        {
+            if (den <= 0) throw new ArgumentException("Знаменатель должен быть больше 0!");
+            this.Numerator = num;
+            this.Denominator = den;
+            this.Value = num / den;
+        }
+    }
+
+
+
+    internal class Program
+    {
+        public static void Main()
+        {
+            Fraction ratio = new Fraction(10, -2);
+            Console.WriteLine($"{ratio.Numerator}/{ratio.Denominator} = {ratio.Value.ToString(CultureInfo.InvariantCulture)} ");
+        }
+    }
+}
