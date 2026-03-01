f827ceb4eb9bdf4cf29e12371e89dcb3c2dc66ea
Владимир Хоцанович <vhotsanovich@yandex.ru>
2026-03-01 13:37:50 +0800

    9.4 readonly поля.12.ChangeColor

--- 9.4 readonly поля.09.Driver/Program.cs
+++ 9.4 readonly поля.09.Driver/Program.cs
@@ -22,10 +22,29 @@
         public Car(string number, string color, Driver driver)
         {
             this.Number = number;
-            this.Color = color; 
+            this.Color = color;
             this.Driver = driver;
         }
     }
+    public class Client
+    {
+        public readonly Guid Id;
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string CardNumber { get; set; }
+
+        public Client(string firstName, string lastName, int age, string cardNumber)
+        {
+            Id = Guid.NewGuid();
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            CardNumber = cardNumber;
+        }
+    }
+
+
 
     internal class Program
     {
@@ -42,13 +61,23 @@
             //carA.Driver = carB.Driver;
             //Console.WriteLine($"Автомобиль A: {carA.Number} {carA.Color} {carA.Driver.LastName}");
 
-            Driver driverA = new Driver("Иван", "Иванов", 34);
-            Driver driverC = new Driver("Пётр", "Петров", 25);
-            Car carA = new Car("A123MP", "Красный", driverA);
-            Car carB = new Car("B456XC", "Синий", null);
-            Car carC = new Car("K789BA", "Белый", driverC);
+            //Driver driverA = new Driver("Иван", "Иванов", 34);
+            //Driver driverC = new Driver("Пётр", "Петров", 25);
+            //Car carA = new Car("A123MP", "Красный", driverA);
+            //Car carB = new Car("B456XC", "Синий", null);
+            //Car carC = new Car("K789BA", "Белый", driverC);
+            //Car[] cars = new Car[] { carA, carB, carC };
+            //CarsDrivers(cars);
+
+            Car carA = new Car("O123MP", "Красный", null);
+            Car carB = new Car("M456XC", "Синий", null);
+            Car carC = new Car("K789BA", "Белый", null);
             Car[] cars = new Car[] { carA, carB, carC };
-            CarsDrivers(cars);
+            ChangeColor(cars);
+            for (int i = 0; i < cars.Length; ++i)
+            {
+                Console.WriteLine($"{cars[i].Color}");
+            }
         }
 
         public static void CarsDrivers(Car[] cars)
@@ -58,6 +87,27 @@
                 string driverLastName = car.Driver is null ? "Нет водителя" : car.Driver.LastName;
                 Console.WriteLine($"{car.Number}:{driverLastName}");
             }
+        }
+        public static void ChangeColor(Car[] cars)
+        {
+
+            foreach (var car in cars)
+            {
+                car.Color = GetColor(car.Number[0]);
+            }
+        }
+
+        private static string GetColor(char c)
+        {
+            string color = "";
+            switch (char.ToLower(c))
+            {
+                case 'o': color =  "Оранжевый"; break;
+                case 'c': color =  "Синий"; break;
+                case 'k': color = "Красный"; break;
+                default: color = "Белый"; break;
+            }
+            return color;
         }
     }
 }
