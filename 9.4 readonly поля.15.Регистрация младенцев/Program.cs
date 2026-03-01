d4bc34157519efe7e8f1a40dd3f720a49d76cc78
Владимир Хоцанович <vhotsanovich@yandex.ru>
2026-03-01 19:52:11 +0800

    Тестировал инлексаторы

---
+++ 9.4 readonly поля.15.Регистрация младенцев/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace _9._4_readonly_поля._15.Регистрация_младенцев
+{
+    public class Person()
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public readonly Person Mom;
+        public readonly Person Dad;
+
+        public Person(string name, int age, Person mom, Person dad):this()
+        { 
+            this.Name = name;
+            this.Age = age;
+            this.Mom = mom;
+            this.Dad = dad;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var mom = new Person("Светлана", 23, null, null);
+            //var dad = new Person("Алексей", 24, null, null);
+
+            //RegisterBaby("Миша", mom, dad);
+            Person mom = new Person("Светлана", 20, null, null);
+            Person dad = new Person("Алексей", 20, null, null);
+            Person baby = new Person("Паша", 0, mom, dad);
+            Console.WriteLine($"Mom age:{baby.Mom.Age}");
+            Console.WriteLine($"Dad age:{baby.Dad.Age}");
+            UpdateMomAge(baby);
+            UpdateDadAge(baby);
+            Console.WriteLine($"Mom age:{baby.Mom.Age}");
+            Console.WriteLine($"Dad age:{baby.Dad.Age}");
+        }
+        public static void RegisterBaby(string name, Person mom, Person dad)
+        {
+            Person baby = new Person(name, 0, mom, dad);
+            Console.WriteLine($"Зарегистрирован новый ребёнок Имя:{baby.Name}, Мама:{baby.Mom.Name}, Папа:{baby.Dad.Name}");
+            Console.WriteLine($"Mom age:{baby.Mom.Age}");
+        }
+
+        public static void UpdateMomAge(Person child)
+        {
+            child.Mom.Age++;
+        }
+        public static void UpdateDadAge(Person child)
+        {
+            child.Dad.Age++;
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,14 +5,14 @@
 //        static void Main(string[] args)
 //        {
 
-            
+
 //            string[] days4 = new string[] { "пн", "вт", "ср", "чт", "пт", "сб", "вс" };
 //            string[] days5 = ["пн", "вт", "ср", "чт", "пт", "сб", "вс" ];
 //            string[] days6 = { "пн", "вт", "ср", "чт", "пт", "сб", "вс" }; // почему так раобтает ?!
 //            string[] days7;
 //            days7 = { "пн", "вт", "ср", "чт", "пт", "сб", "вс" }; // не работает
 //            days7 = [ "пн", "вт", "ср", "чт", "пт", "сб", "вс" ]; // работает
-            
+
 //            var days1 = new Dictionary<string, int>() // как эти скобки вписываются в логишу присвоения множеств ? Шиворот на выворот же присваивается
 //            {
 //                ["пн"] = 0,
@@ -38,24 +38,71 @@
 //}
 
 
+//public class User
+//{
+//    private User field;
+
+//    public string Name { get; set; }
+//    public void Method1(User user)
+//    {
+//        field = user;
+//        field.Name = "NoName"; // Нет ошибки
+//    }
+//}
+
+//internal class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        var v = new User();
+//        var v1 = new User();
+//        v.Method1(v1);
+//    }
+//}
 public class User
 {
-    public User field;
-
-    public string Name { get; set; }
-    public void Method1()
+    public string Name { get; }
+    public int Age { get; }
+    public User(string name, int age)
     {
-        field.Name = "NoName"; // ?
+        Name = name;
+        Age = age;
     }
 }
 
-internal class Program
+public class Company
 {
-    static void Main(string[] args)
+    private User[] users;
+
+    public Company()
     {
-        var v = new User();
-        v.Method1();
-        Console.WriteLine(v.field.Name);
-        v.field.Name = "dfdf";
+        users = new User[]
+        {
+            new User("Иосиф", 25),
+            new User("Тимур", 28),
+            new User("Вася", 18),
+            new User("Таня", 32)
+        };
+    }
+public User this[int index]
+    {
+        get { return users[index]; }
+        set { users[index] = value; }
+    }
+
+
+}
+
+internal class Programm
+{
+    static void Main()
+    {
+        var user1 = new User("AAAA", 1);
+        var user2 = new User("BBB", 3);
+        var company = new Company();
+        Console.WriteLine($"{company[0].Name} : {company[0].Age}");
+        company[0] = user1;
+        Console.WriteLine($"{company[0].Name} : {company[0].Age}");
+
     }
 }
