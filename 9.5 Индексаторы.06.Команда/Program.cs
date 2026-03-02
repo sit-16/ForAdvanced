770a1c19b88ada2db47debf993cf47d6b5e8f754
Vovan <vhotsanovich@yandex.ru>
2026-03-02 22:46:59 +0800

    9.5 Индексаторы.06.Команда

---
+++ 9.5 Индексаторы.06.Команда/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _9._5_Индексаторы._06.Команда
+{
+    public class Team
+    {
+        Player[] players;
+        public Team()
+        {
+            players = new Player[10];
+        }
+        public Player this[int index] 
+        {
+            get { return players[index]; }
+            set { players[index] = value; }
+        }
+    }
+
+    public class Player
+    {
+        public string Name { get; }
+        public int Number { get; }
+        public Player(string name, int number)
+        {
+            Name = name;
+            Number = number;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Team realMadrid = new Team();
+            realMadrid[0] = new Player("Benzema", 9);
+            realMadrid[1] = new Player("Sergio Ramos", 4);
+
+            Console.WriteLine(realMadrid[1].Name);
+        }
+    }
+}
