0444dc64ca2017376a07b482ef4be471d5c0e730
Vovan <vhotsanovich@yandex.ru>
2026-03-02 23:13:17 +0800

    9.5 Индексаторы.07.Команда2

---
+++ 9.5 Индексаторы.07.Команда2/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace _9._5_Индексаторы._07.Команда2
+{
+    internal class Program
+    {
+        public class Team
+        {
+            Player[] players;
+            public Team()
+            {
+                players = new Player[10];
+            }
+            public Player this[int index]
+            {
+                get 
+                {
+                    if (index >= 0 && index < players.Length)
+                        return players[index];
+                    else throw new Exception("Ошибка");
+
+                }
+                set 
+                {
+                    if (index >= 0 && index < players.Length)
+                        players[index] = value;
+                    else throw new Exception("Ошибка");
+                }
+            }
+        }
+
+        public class Player
+        {
+            public string Name { get; }
+            public int Number { get; }
+            public Player(string name, int number)
+            {
+                Name = name;
+                Number = number;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Team realMadrid = new Team();
+
+            realMadrid[0] = new Player("Benzema", 9);
+            realMadrid[1] = new Player("Sergio Ramos", 4);
+            realMadrid[12] = new Player("Sergio Ramos2", 43);
+            Console.WriteLine(realMadrid[2].Name);
+        }
+    }
+}
