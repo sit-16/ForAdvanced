9ac1644b518e2119852ccd48ddc22c752c068579
Vovan <vhotsanovich@yandex.ru>
2026-03-05 22:59:41 +0800

    10.2 Иерархия наследования.04.Создаем животных

---
+++ 10.2 Иерархия наследования.04.Создаем животных/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _10._2_Иерархия_наследования._04.Создаем_животных
+{
+    public class Animal
+    {
+        public void Breathe() { }
+        public void Eat() { }
+    }
+    public class Fish : Animal
+    {
+        public void Swim() { }
+    }
+
+    public class Bird : Animal
+    {
+        public void LayEggs() { }
+    }
+    public class FlyingBird : Bird
+    {
+        public void Fly() { }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new Animal();
+            var b = new Animal();
+        }
+    }
+}
