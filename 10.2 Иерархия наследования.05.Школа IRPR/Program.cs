b27d6372288906434b0b9c6a5bc3c5e96d27db76
Vovan <vhotsanovich@yandex.ru>
2026-03-06 23:47:55 +0800

    10.2 Иерархия наследования.05.Школа IRPR

---
+++ 10.2 Иерархия наследования.05.Школа IRPR/Program.cs
@@ -0,0 +1,35 @@
+﻿using static _10._2_Иерархия_наследования._05.Школа_IRPR.Program;
+
+namespace _10._2_Иерархия_наследования._05.Школа_IRPR
+{
+    internal class Program
+    {
+        public class User
+        {
+            public void Solve(int n) { }
+        }
+
+        public class Student : User
+        { }
+
+        public class Teacher: User
+        {
+            public void CheckSolution(User user, int n) { }
+        }
+
+        public class Admin : User 
+        {
+            public void Edit(int n) { }
+        }
+
+        public class SuperAdmin : Admin
+        { 
+            public void Grant(User user){ }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
