9dca2578b1aad15ef9bef5841f59e3a2d8625f25
Vovan <vhotsanovich@yandex.ru>
2026-03-10 22:16:39 +0800

    11.1 Upcast, DownCast.15.FindFirstTriangle

--- 11.1 Upcast, DownCast.14.FindCircles/Program.cs
+++ 11.1 Upcast, DownCast.14.FindCircles/Program.cs
@@ -18,22 +18,53 @@
             //{
             //    Console.WriteLine($"Radius: {c.Radius}");
             //}
-            Circle circle = new Circle(5, "r", -1, "d", new Point(3, 5), 4);
-            Edge edge = new Edge(0, "t", 5.6, "d", new Point(1, 2), new Point(5, -4));
 
-            Polygon polygon1 = new Polygon(3, "g", 6.5, "p", new List<Point> { new Point(5, 8), new Point(1, 7), new Point(4, 5) });
-            Polygon polygon2 = new Polygon(2, "d", 6.7, "y", new List<Point> { new Point(1, -6), new Point(1, 6), new Point(5.6, 6), new Point(4, 7) });
-            Polygon polygon = new Polygon(3, "d", 1.7, "k", new List<Point> { new Point(2, 3), new Point(5, 6), new Point(4, 9) });
 
-            Edge edge2 = new Edge(1, "f", 7.8, "r", new Point(2, 3), new Point(4, -6));
-            Circle circle2 = new Circle(89, "n", 3, "p", new Point(1, 7), 6);
 
-            Figure[] figures = new Figure[] { circle, edge, circle2, polygon2, polygon, edge2, polygon1 };
+            //Circle circle = new Circle(5, "r", -1, "d", new Point(3, 5), 4);
+            //Edge edge = new Edge(0, "t", 5.6, "d", new Point(1, 2), new Point(5, -4));
 
-            RepaintEdges(figures);
-            Console.WriteLine(edge.EdgeColor);
-            Console.WriteLine(edge2.EdgeColor);
+            //Polygon polygon1 = new Polygon(3, "g", 6.5, "p", new List<Point> { new Point(5, 8), new Point(1, 7), new Point(4, 5) });
+            //Polygon polygon2 = new Polygon(2, "d", 6.7, "y", new List<Point> { new Point(1, -6), new Point(1, 6), new Point(5.6, 6), new Point(4, 7) });
+            //Polygon polygon = new Polygon(3, "d", 1.7, "k", new List<Point> { new Point(2, 3), new Point(5, 6), new Point(4, 9) });
+
+            //Edge edge2 = new Edge(1, "f", 7.8, "r", new Point(2, 3), new Point(4, -6));
+            //Circle circle2 = new Circle(89, "n", 3, "p", new Point(1, 7), 6);
+
+            //Figure[] figures = new Figure[] { circle, edge, circle2, polygon2, polygon, edge2, polygon1 };
+
+            //RepaintEdges(figures);
+            //Console.WriteLine(edge.EdgeColor);
+            //Console.WriteLine(edge2.EdgeColor);
+
+            Circle firstCircle = new Circle(5, "r", -1, "d", new Point(3, 5), 4);
+            Edge firstEdge = new Edge(0, "t", 5.6, "d", new Point(1, 2), new Point(5, -4));
+            Polygon firstPolygon = new Polygon(3, "d", 1.7, "k", new List<Point> { new Point(1, 7), new Point(4, 5) });
+            Polygon secondPolygon = new Polygon(3, "g", 6.5, "p", new List<Point> { new Point(5, 8), new Point(1, 7), new Point(4, 5) });
+            Polygon thirdPolygon = new Polygon(2, "d", 6.7, "y", new List<Point> { new Point(1, -6), new Point(1, 6), new Point(5.6, 6), new Point(4, 7) });
+            Polygon fourthPolygon = new Polygon(3, "d", 1.7, "k", new List<Point> { new Point(2, 3), new Point(5, 6), new Point(4, 9) });
+            Edge secondEdge = new Edge(1, "f", 7.8, "r", new Point(2, 3), new Point(4, -6));
+            Circle secondCircle = new Circle(89, "n", 3, "p", new Point(1, 7), 6);
+
+            Polygon firstTriangle = FindFirstTriangle(new Figure[] { firstCircle, firstEdge, firstPolygon, secondPolygon, thirdPolygon, fourthPolygon, secondEdge, secondCircle });
+
+            Console.WriteLine($"Слой:{firstTriangle.Layer}, Цвет границ:{firstTriangle.EdgeColor}, Толщина границ:{firstTriangle.EdgeThickness}, Цвет заливки:{firstTriangle.FillColor}");
+
         }
+
+
+        public static Polygon FindFirstTriangle(Figure[] figures)
+        {
+            Polygon FirstTriangle;
+            foreach (var f in figures)
+            {
+                FirstTriangle = f as Polygon;
+                if (FirstTriangle != null)
+                    if (FirstTriangle.Points.Count == 3) return FirstTriangle;
+            }
+            return null;
+        }
+
         public static void RepaintEdges(Figure[] figures)
         {
             foreach (var f in figures)
