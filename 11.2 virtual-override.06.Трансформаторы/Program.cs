cc4fc4cb212c5c9e0b9ca4def2485e50ce1b1cdb
Vovan <vhotsanovich@yandex.ru>
2026-03-11 21:26:36 +0800

    11.2 virtual-override.06.Трансформаторы

---
+++ 11.2 virtual-override.06.Трансформаторы/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _11._2_virtual_override._06.Трансформаторы
+{
+    public class Summator
+    {
+        protected virtual int Transform(int item)
+        {
+            return item;
+        }
+
+        public int Sum(int N)
+        {
+            int result = 0;
+            for (int i = 1; i <= N; i++)
+            {
+                result += this.Transform(i);
+            }
+            return result;
+        }
+    }
+
+    public class SquareSummator: Summator
+    {
+        protected override int Transform(int item)
+        {
+            return item * item;
+        }
+    }
+
+    public class CubeSummator: Summator
+    {
+        protected override int Transform(int item)
+        {
+            return item * item * item;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Summator().Sum(5));
+            Console.WriteLine(new SquareSummator().Sum(5));
+            Console.WriteLine(new CubeSummator().Sum(5));
+        }
+    }
+}
