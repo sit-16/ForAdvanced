b252b295d80a82b818bf723c860fde9d7d816466
Vovan <vhotsanovich@yandex.ru>
2026-03-11 20:52:14 +0800

    11.2 virtual-override.05.Даты

---
+++ 11.2 virtual-override.05.Даты/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace _11._2_virtual_override._05.Даты
+{
+    public class BaseDate
+    { 
+        public int Day { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
+        public BaseDate(int year, int month, int day)
+        {
+            Day = day;
+            Month = month;
+            Year = year;
+        }
+        public virtual string GetFormat()
+        {
+            return $"год:{Year}, месяц:{Month:00}, день:{Day:00}";
+        }
+    }
+
+    public class AmericanDate : BaseDate
+    {
+        public AmericanDate(int year, int month, int day):base (year,month, day)
+        {            
+        }
+        public override string GetFormat()
+        {
+            return $"{Month:00}.{Day:00}.{Year}";
+        }
+    }
+
+    public class EuropeanDate : BaseDate
+    {
+        public EuropeanDate(int year, int month, int day) : base(year, month, day)
+        {
+        }
+        public override string GetFormat()
+        {
+            return $"{Day:00}.{Month:00}.{Year}";
+        }
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseDate date = new BaseDate(2021, 3, 24);
+            AmericanDate date1 = new AmericanDate(2021, 3, 24);
+            EuropeanDate date2 = new EuropeanDate(2021, 3, 24);
+
+            List<BaseDate> dates = new List<BaseDate>
+            {
+                date,date1, date2
+            };
+
+            foreach (var item in dates)
+            {
+                Console.WriteLine(item.GetFormat());
+            }
+        }
+    }
+}
--- 11.2 virtual-override/Program.cs
+++ 11.2 virtual-override/Program.cs
@@ -19,40 +19,43 @@ namespace CSharpEssentials
         public int Course { get; set; }
         public string Profession { get; set; }
 
-        public override string GetInfo()
+        public new string GetInfo()
         {
             return $"{base.GetInfo()}, Курс: {Course}, Специальность: {Profession}";
         }
 
     }
-    public class ProgrammingStudent : Student
-    {
-        public string ProgrammingLanguage { get; set; }
+    //public class ProgrammingStudent : Student
+    //{
+    //    public string ProgrammingLanguage { get; set; }
 
-        public override string GetInfo()
-        {
-            return $"{base.GetInfo()}, язык программирования: {ProgrammingLanguage}";
-        }
-    }
+    //    public override string GetInfo()
+    //    {
+    //        return $"{base.GetInfo()}, язык программирования: {ProgrammingLanguage}";
+    //    }
+    //}
 
-    public class Professor : Person
-    {
-        public string Subject { get; set; }
-        public override string GetInfo()
-        {
-            return $"{base.GetInfo()}, предмет: {Subject}";
-        }
-    }
+    //public class Professor : Person
+    //{
+    //    public string Subject { get; set; }
+    //    public override string GetInfo()
+    //    {
+    //        return $"{base.GetInfo()}, предмет: {Subject}";
+    //    }
+    //}
 
     class Program
     {
         static void Main()
         {
             List<Person> people = new List<Person>();
-            people.Add(new Student() { Name = "Student1", Age = 21, Course = 1, Profession = "C#" });
-            people.Add(new Professor() { Name = "Professor1", Age = 51, Subject = "Матан" });
-            people.Add(new ProgrammingStudent() { Name = "ProgrammingStudent1", Age = 22, Course = 2, Profession = "ЯП", ProgrammingLanguage = "C#" });
+            Student stud = new Student() { Name = "Student1", Age = 21, Course = 1, Profession = "C#" };
+            people.Add(stud);
+            //people.Add(new Professor() { Name = "Professor1", Age = 51, Subject = "Матан" });
+            //people.Add(new ProgrammingStudent() { Name = "ProgrammingStudent1", Age = 22, Course = 2, Profession = "ЯП", ProgrammingLanguage = "C#" });
 
+
+            Console.WriteLine(stud.GetInfo());
             foreach (var person in people)
             {
                 Console.WriteLine(person.GetInfo());
