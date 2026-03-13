9d517ed10bf1867815c0f3f22c559300ba7fecc5
Vovan <vhotsanovich@yandex.ru>
2026-03-13 22:42:08 +0800

    11.3 Базовый класс Object.08.Вывод футболиста 2

---
+++ 11.3 Базовый класс Object.08.Вывод футболиста 2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _11._3_Базовый_класс_Object._08.Вывод_футболиста_2
+{
+    public class Player
+    {
+        public string Name { get; }
+        public int Number { get; }
+        public Player(string name, int number)
+        {
+            Name = name;
+            Number = number;
+        }
+        public override string ToString()
+        {
+            return $"{this.Name} играет под номером {this.Number}";
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player("Sergio Ramos", 4);
+            Console.WriteLine(player);
+        }
+    }
+}
