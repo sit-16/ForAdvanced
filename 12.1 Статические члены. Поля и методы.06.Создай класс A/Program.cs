6e56832b25c08051e0a9cb59ab5d8c0d13db8013
Vovan <vhotsanovich@yandex.ru>
2026-03-15 23:09:45 +0800

    12.1 Статические члены. Поля и методы.06.Создай класс A

---
+++ 12.1 Статические члены. Поля и методы.06.Создай класс A/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _12._1_Статические_члены._Поля_и_методы._06.Создай_класс_A
+{
+    public class A
+    {
+        public static int Field1;// { get; set; }
+        public string Field2;
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            A.Field1 = 1;
+            A a = new A();
+            a.Field2 = "*";
+
+            Console.WriteLine("Field1:{0}, Field2:{1}", A.Field1, a.Field2);
+        }
+    }
+}
