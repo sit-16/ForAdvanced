7cb2ce4757b9424e6f08d8db1953ff2467467f14
Vovan <vhotsanovich@yandex.ru>
2026-03-16 22:09:00 +0800

    12.1 Статические члены. Поля и методы.09.Класс Constants

---
+++ 12.1 Статические члены. Поля и методы.09.Класс Constants/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _12._1_Статические_члены._Поля_и_методы._09.Класс_Constants
+{
+    public class Constants
+    {
+        public static int TimeoutSeconds { get; set; }
+        public static string AdminLogin {  get; set; }
+        public static string AdminPassword { get; set; }
+        public static bool Development { get; set; }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
