155cf001b9f5d219be93d0c840bf681c30354c25
Vovan <vhotsanovich@yandex.ru>
2026-03-16 22:24:59 +0800

    12.1 Статические члены. Поля и методы.09.Класс Point (2)

--- 12.1 Статические члены. Поля и методы.09.Класс Point (1)/Program.cs
+++ 12.1 Статические члены. Поля и методы.09.Класс Point (1)/Program.cs
@@ -9,8 +9,13 @@ namespace _12._1_Статические_члены._Поля_и_методы._09
         public int Y { get; }
         public Point(int x, int y)
         {
-            Counter++;
+            Counter ++;
             X = x; Y = y;
+        }
+
+        public static double LengthPoints(Point p1, Point p2)
+        {
+            return Math.Sqrt(Math.Pow((p1.X - p2.X), 2) + Math.Pow((p1.Y - p2.Y), 2));
         }
     }
 
@@ -23,6 +28,10 @@ namespace _12._1_Статические_члены._Поля_и_методы._09
 
             Point point2 = new Point(2, 3);
             Console.WriteLine(Point.Counter);
+            Point point3 = new Point(1, 5);
+            Point point4 = new Point(5, 5);
+
+            Console.WriteLine(Point.LengthPoints(point3, point4));
         }
     }
 }
