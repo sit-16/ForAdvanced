d8d0e719f651b2b13662533f22a3ed441ff2b2a6
Vovan <vhotsanovich@yandex.ru>
2026-03-16 22:19:34 +0800

    12.1 Статические члены. Поля и методы.09.Класс Point (1)

---
+++ 12.1 Статические члены. Поля и методы.09.Класс Point (1)/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace _12._1_Статические_члены._Поля_и_методы._09.Класс_Point__1_
+{
+    public class Point 
+    {
+        public static int Counter { get; set; }
+        public int X { get; }
+        public int Y { get; }
+        public Point(int x, int y)
+        {
+            Counter++;
+            X = x; Y = y;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point point1 = new Point(1, 1);
+            Console.WriteLine(Point.Counter);
+
+            Point point2 = new Point(2, 3);
+            Console.WriteLine(Point.Counter);
+        }
+    }
+}
