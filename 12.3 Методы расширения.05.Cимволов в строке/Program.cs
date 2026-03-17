6d05babff9ada55570fd50c9f8170dc2890875fa
Vovan <vhotsanovich@yandex.ru>
2026-03-17 23:26:04 +0800

    12.3 Методы расширения.05.Cимволов в строке

---
+++ 12.3 Методы расширения.05.Cимволов в строке/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _12._3_Методы_расширения._05.Cимволов_в_строке
+{
+    public static class StringHelpers
+    {
+        public static int CharCount(this string s, char c)
+        {
+            int counter = 0;
+            foreach (var ch in s)
+            {
+                if (ch == c) counter++;
+            }
+            return counter;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Hello world";
+            char c = 'l';
+            int i = s.CharCount(c);
+            Console.WriteLine(i);
+        }
+    }
+}
