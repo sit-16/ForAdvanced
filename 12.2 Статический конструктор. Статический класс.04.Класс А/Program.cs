665cbd116dec225ab9750acbf7603b4542471061
Vovan <vhotsanovich@yandex.ru>
2026-03-17 22:43:40 +0800

    12.2 Статический конструктор. Статический класс.04.Класс А

---
+++ 12.2 Статический конструктор. Статический класс.04.Класс А/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _12._2_Статический_конструктор._Статический_класс._04.Класс_А
+{
+    public class A
+    {
+        static A()
+        {
+            Console.WriteLine("Да! Я понял как создавать статический конструктор!");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           var a = new A();
+        }
+    }
+}
