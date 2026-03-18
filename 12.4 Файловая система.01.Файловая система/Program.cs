64ab99a14822f6366d7ff10123be3c2a781ff94f
Vovan <vhotsanovich@yandex.ru>
2026-03-18 23:28:50 +0800

    DriveFormat == "NTFS"

--- 12.4 Файловая система.01.Файловая система/Program.cs
+++ 12.4 Файловая система.01.Файловая система/Program.cs
@@ -5,15 +5,21 @@
     {
         static void Main(string[] args)
         {
+            const int width = 8;
+            Console.WriteLine($"{"Имя",-width}|" +
+                    $" {"Формат",-width}|" +
+                    $"{"Тип",-width}|" +
+                    $"{"Объём Гб",width}|" +
+                    $"{"Free Гб",width}|" +
+                    $"{"Free %",width}|");
             var driveInfo = DriveInfo.GetDrives();
             foreach (var dr in driveInfo)
             {
-                if (dr.IsReady) Console.WriteLine($"{dr.Name,5}|" +
-                    $" {dr.DriveFormat,-10}|{dr.DriveType,7}|" +
-                    $"{dr.TotalSize/Math.Pow(1024,3),7:f2} Гб|" +
-                    $"{dr.TotalFreeSpace / Math.Pow(1024, 3),7:f2} Гб|" +
-                    $"{((double)dr.TotalFreeSpace/ dr.TotalSize),7:P2}|");
-                else Console.WriteLine($"{dr.Name,5}| {"Not ready",-10}");
+                if (dr.IsReady && dr.DriveFormat == "NTFS") Console.WriteLine($"{dr.Name,-width}|" +
+                    $" {dr.DriveFormat,-width}|{dr.DriveType,-width}|" +
+                    $"{dr.TotalSize/Math.Pow(1024,3),width:f2}|" +
+                    $"{dr.TotalFreeSpace / Math.Pow(1024, 3),width:f2}|" +
+                    $"{((double)dr.TotalFreeSpace/ dr.TotalSize),width:P2}|");
             }
             
         }
