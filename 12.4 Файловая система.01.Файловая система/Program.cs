0463aa0fadb3587a19db8ce8784685156bd08323
Vovan <vhotsanovich@yandex.ru>
2026-03-18 23:07:46 +0800

    12.4 Файловая система.01.DriveInfo

---
+++ 12.4 Файловая система.01.Файловая система/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _12._4_Файловая_система._01.Файловая_система
+{
+    using System.IO;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var driveInfo = DriveInfo.GetDrives();
+            foreach (var dr in driveInfo)
+            {
+                if (dr.IsReady) Console.WriteLine($"{dr.Name,5}|" +
+                    $" {dr.DriveFormat,-10}|{dr.DriveType,7}|" +
+                    $"{dr.TotalSize/Math.Pow(1024,3),7:f2} Гб|" +
+                    $"{dr.TotalFreeSpace / Math.Pow(1024, 3),7:f2} Гб|" +
+                    $"{((double)dr.TotalFreeSpace/ dr.TotalSize),7:P2}|");
+                else Console.WriteLine($"{dr.Name,5}| {"Not ready",-10}");
+            }
+            
+        }
+    }
+}
