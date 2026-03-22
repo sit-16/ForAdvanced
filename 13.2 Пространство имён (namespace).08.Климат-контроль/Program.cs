4523801a9e150ecaa70d5e158c73d90dbc2aeb00
Vovan <vhotsanovich@yandex.ru>
2026-03-22 20:14:24 +0800

    13.2 Пространство имён (namespace).08.Климат-контроль

---
+++ 13.2 Пространство имён (namespace).08.Климат-контроль/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace SmartHome.Logic
+{
+    public class Conditioner
+    {
+        public string Cool()
+        {
+            return "Охлаждение запущено";
+        }
+    }
+}
+namespace SmartHome.Logic
+{
+    public class Thermostat
+    {
+        public void Start()
+        {
+// Создайте объект класса Conditioner с именем conditioner 
+Conditioner conditioner = new Conditioner();
+// Выведите результат метода Cool() в консоль
+Console.WriteLine(conditioner.Cool());
+        }
+    }
+}
