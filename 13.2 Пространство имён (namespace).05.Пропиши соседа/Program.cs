2dd49cec8b67fc2d40571f6581c02a66a1b4bc82
Vovan <vhotsanovich@yandex.ru>
2026-03-22 18:48:14 +0800

    13.2 Пространство имён (namespace).05.Пропиши соседа

---
+++ 13.2 Пространство имён (namespace).05.Пропиши соседа/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace OnlineShop.Web
+{
+    // класс модели относящийся к работе web-приложения
+    public class Order
+    {
+    }
+}
+namespace OnlineShop.Db
+{
+    // класс модели относящийся к работе с базой данных
+    public class Order
+    {
+    }
+}
+
+namespace _13._2_Пространство_имён__namespace_._05.Пропиши_соседа
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            OnlineShop.Web.Order webOrder = new OnlineShop.Web.Order();
+            OnlineShop.Db.Order dbOrder = new OnlineShop.Db.Order();
+        }
+    }
+}
+
