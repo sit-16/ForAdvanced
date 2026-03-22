f63a5489d442547d0f1a9ee2913e7339d093dad0
Vovan <vhotsanovich@yandex.ru>
2026-03-22 18:59:42 +0800

    13.2 Пространство имён (namespace).07.Взгляд из другой Галактики

---
+++ 13.2 Пространство имён (namespace).07.Взгляд из другой Галактики/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Andromeda.DeepSpace
+{
+    public class Star
+    {
+        public string Observe()
+        {
+            return "Supernova detected";
+        }
+    }
+}
+
+namespace Earth.Observatory
+{
+    public class Telescope
+    {
+        public static void Main()
+        {
+            // Создайте переменную-объект newStar класса Star 
+            Andromeda.DeepSpace.Star newStar = new Andromeda.DeepSpace.Star();
+            // Выведите результат работы метода Observe() в консоль
+            Console.WriteLine(newStar.Observe());
+        }
+    }
+}
