e844e0bfd21c934a3b94120541433bebc12641f5
Vovan <vhotsanovich@yandex.ru>
2026-03-23 21:12:04 +0800

    13.3 Алиасы (псевдонимы).08.Активация охраны

---
+++ 13.3 Алиасы (псевдонимы).08.Активация охраны/Program.cs
@@ -0,0 +1,26 @@
+﻿using SecuritySensor = Global.Security.Devices.Sensor;
+using Printer = System.Console;
+
+
+namespace Global.Security.Devices
+{ 
+    public class Sensor
+    {
+        public string GetStatus()
+        {
+            return "Охрана включена";
+        }
+    }
+}
+
+namespace CSharpEssentials
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            SecuritySensor sensor = new SecuritySensor();
+            Printer.WriteLine(sensor.GetStatus());
+        }
+    }
+}
