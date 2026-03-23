87117e233424bd4b3e0d7e64b1007456bf4d6174
Vovan <vhotsanovich@yandex.ru>
2026-03-23 23:29:45 +0800

    13.3 Алиасы (псевдонимы).11.Жизненный цикл заказа

---
+++ 13.3 Алиасы (псевдонимы).11.Жизненный цикл заказа/Program.cs
@@ -0,0 +1,48 @@
+﻿// 1. Создай алиас OrderStatus для вложенного типа
+using OrderStatus = Store.Logic.Order.Status;
+// 2. Добавь статический импорт для этого же типа
+using static Store.Logic.Order.Status;
+
+namespace Store.Logic
+{
+    public class Order
+    {
+        public class Status
+        {
+            private string _name;
+
+            private Status(string name)
+            {
+                _name = name;
+            }
+
+            public static readonly Status Created = new Status("Created");
+            public static readonly Status Processing = new Status("Processing");
+            public static readonly Status Shipped = new Status("Shipped");
+            public static readonly Status Delivered = new Status("Delivered");
+
+
+            public override string ToString()
+            {
+                return _name;
+            }
+        }
+    }
+}
+
+namespace StoreApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Если алиас и импорт настроены верно, код ниже заработает
+            OrderStatus current = Shipped;
+
+            if (current == Shipped)
+            {
+                Console.WriteLine(current);
+            }
+        }
+    }
+}
