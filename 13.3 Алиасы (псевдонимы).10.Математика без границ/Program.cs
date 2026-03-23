f673c876776247e0a65288fefaaf5dda9982d4f7
Vovan <vhotsanovich@yandex.ru>
2026-03-23 21:37:42 +0800

    13.3 Алиасы (псевдонимы).10.Математика без границ

---
+++ 13.3 Алиасы (псевдонимы).10.Математика без границ/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+// Подключи статический импорт требуемого класса
+using static System.Math;
+
+public static class DataStorage 
+{
+    public static int Value { get;} = 4;
+}
+
+namespace CSharpEssentials
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sqrt(DataStorage.Value));
+        }
+
+    }
+}
