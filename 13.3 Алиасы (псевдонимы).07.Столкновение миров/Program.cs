1a84a657ae183bc3de9948f1c4a9dafb349f3ab3
Vovan <vhotsanovich@yandex.ru>
2026-03-23 20:55:43 +0800

    13.3 Алиасы (псевдонимы).07.Столкновение миров

---
+++ 13.3 Алиасы (псевдонимы).07.Столкновение миров/Program.cs
@@ -0,0 +1,38 @@
+﻿using DbUser = Database.Models.User;
+using ApiUser = External.Api.User;
+
+namespace Database.Models
+{
+    public class User
+    {
+        public string GetInfo()
+        {
+            return "DB_Record";
+        }
+    }
+}
+namespace External.Api
+{
+    public class User
+    {
+        public string GetInfo()
+        {
+            return "API_Data";
+        }
+    }
+}
+
+
+
+namespace CSharpEssentialsв
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DbUser model = new DbUser();
+            ApiUser profile = new ApiUser();
+            Console.WriteLine($"{model.GetInfo()} | {profile.GetInfo()}");
+        }
+    }
+}
