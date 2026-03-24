2b814eb2be90064954941f3fa7c3c97c95955efc
Vovan <vhotsanovich@yandex.ru>
2026-03-24 23:28:31 +0800

    14.1 Операторы, работающие с NULL.08.Упрости код

---
+++ 14.1 Операторы, работающие с NULL.08.Упрости код/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _14._1_Операторы__работающие_с_NULL._08.Упрости_код
+{
+
+    internal class Program
+    {
+        static void Print(string name)
+        {
+            //Console.WriteLine(name??"Неизвестное имя");
+
+            if (name == null)
+            {
+                Console.WriteLine("Неизвестное имя");
+            }
+            else
+            {
+                Console.WriteLine(name);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
