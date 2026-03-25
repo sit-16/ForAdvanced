f45dbd51db0b0ae0ca3a60b3ccdb7faf9964fb2b
Vovan <vhotsanovich@yandex.ru>
2026-03-25 21:43:39 +0800

    14.2 Ключевое слово var.06.Анонимный тип

---
+++ 14.2 Ключевое слово var.06.Анонимный тип/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Xml.Linq;
+
+namespace _14._2_Ключевое_слово_var._06.Анонимный_тип
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Анонимный тип
+            var t = new { Name = "Test", Age = 18 };
+            //Массив анонимных объектов анонимного типа
+            var tables = new[] { 
+                new {Name = "Test1", Age = 19 },
+                new {Name = "Test2", Age = 20 }
+            };
+
+        }
+    }
+}
