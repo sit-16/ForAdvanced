3abfcad545b04da1e3b38d623b5ff60587b63af0
Vovan <vhotsanovich@yandex.ru>
2026-03-27 22:06:20 +0800

    16.1 Делегаты.04.Создание своего делегата

---
+++ 16.1 Делегаты.04.Создание своего делегата/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _16._1_Делегаты._04.Создание_своего_делегата
+{
+    internal class Program
+    {
+        delegate bool Predicate(int x);
+        static bool IsAdult(int age)
+        { 
+            return age >= 18;
+        }
+
+        static void Main(string[] args)
+        {
+            var x = 25;
+            Predicate predicate = IsAdult;
+            var result = predicate?.Invoke(x);
+            Console.WriteLine(result);
+        }
+    }
+}
