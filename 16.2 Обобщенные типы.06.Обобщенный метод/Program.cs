f199a63ee9e9fcf4c76fa08e6abaf4dcdcea3982
Vovan <vhotsanovich@yandex.ru>
2026-04-02 23:03:16 +0800

    16.2 Обобщенные типы.06.Обобщенный метод

---
+++ 16.2 Обобщенные типы.06.Обобщенный метод/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _16._2_Обобщенные_типы._06.Обобщенный_метод
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] names = { "Josef", "Vadim", "Ivan" };
+            PrintArray(names);
+        }
+
+        static void PrintArray<T>(T[] items)
+        {
+            foreach (var item in items)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
