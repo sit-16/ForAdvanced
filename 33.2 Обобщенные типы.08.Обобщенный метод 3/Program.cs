bbd6a95e9352592e415611039d44b8f0752e680c
Vovan <vhotsanovich@yandex.ru>
2026-04-03 22:40:46 +0800

    16.2 Обобщенные типы.08.Обобщенный метод 3

---
+++ 33.2 Обобщенные типы.08.Обобщенный метод 3/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _33._2_Обобщенные_типы._08.Обобщенный_метод_3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+            Reverse(numbers);
+            Console.WriteLine(string.Join(", ", numbers));
+
+            List<string> names = new List<string> { "Josef", "Vadim", "Ivan" };
+            Reverse(names);
+            Console.WriteLine(string.Join(", ", names));
+        }
+
+        static void Reverse<T>(List<T> array)
+        {
+            array.Reverse();
+        }
+    }
+}
