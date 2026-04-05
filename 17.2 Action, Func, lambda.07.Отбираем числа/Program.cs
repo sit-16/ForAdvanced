1d68db71dc03a2460047d63c171aac133cf5df92
Vovan <vhotsanovich@yandex.ru>
2026-04-05 22:24:12 +0800

    17.2 Action, Func, lambda.07.Отбираем числа

---
+++ 17.2 Action, Func, lambda.07.Отбираем числа/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _17._2_Action__Func__lambda._07.Отбираем_числа
+{
+    class Program
+    {
+        static void Main()
+        {
+            Print(new List<int> { 1, 2, 3, 4, 5 }, IsEven);
+        }
+
+        static void Print(List<int> items, Func<int, bool> predicate)
+        {
+            foreach (var item in items)
+            {
+                if (predicate(item))
+                {
+                    Console.Write(item + " ");
+                }
+            }
+        }
+
+        private static bool IsEven(int n)
+        {
+            return n % 2 == 0;
+        }
+
+    }
+}
