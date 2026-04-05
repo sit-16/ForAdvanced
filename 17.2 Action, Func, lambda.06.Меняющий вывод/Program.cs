8ad35f14aa77e79661514874821ae2fbc9682d43
Vovan <vhotsanovich@yandex.ru>
2026-04-05 22:07:17 +0800

    17.2 Action, Func, lambda.06.Меняющий вывод

---
+++ 17.2 Action, Func, lambda.06.Меняющий вывод/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace _17._2_Action__Func__lambda._06.Меняющий_вывод
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Print(new List<int> { 1, 2, 3, 4, 5 }, Twice);
+        }
+
+        static void Print(List<int> items, Action<int> action)
+        {
+            foreach (var item in items)
+            {
+                action(item);
+            }
+        }
+
+        private static void Twice(int n)
+        {
+            Console.Write( " {0}", n *= 2);
+        }
+    }
+}
