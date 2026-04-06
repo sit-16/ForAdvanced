84339fd55b1347fb58ae4a41011411e2ee35c896
Vovan <vhotsanovich@yandex.ru>
2026-04-06 23:24:53 +0800

    17.2 Action, Func, lambda.12.Числа по условию 3

---
+++ 17.2 Action, Func, lambda.12.Числа по условию 3/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _17._2_Action__Func__lambda._12.Числа_по_условию_3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Func<int, bool> predicate = n => n * 2 % 10 == 6;
+
+            Print(new List<int> { 1, 2, 3, 4, 5, 7, 13, 27 }, predicate);
+        }
+
+        static void Print(List<int> items, Func<int, bool> predicate)
+        {
+            foreach (var item in items)
+            {
+                if (predicate(item))
+                {
+                    Console.Write(item + " ");
+                }
+            }
+        }
+    }
+}
