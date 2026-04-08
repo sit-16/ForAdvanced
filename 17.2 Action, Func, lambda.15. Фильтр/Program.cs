e15b5560d26941e5740c71f85af1c080b45e8f8f
Vovan <vhotsanovich@yandex.ru>
2026-04-08 20:52:23 +0800

    17.2 Action, Func, lambda.15. Фильтр

---
+++ 17.2 Action, Func, lambda.15. Фильтр/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stepik
+{
+
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<User> users = new List<User>
+            {
+            new User { Name = "Alice", Age = 25 },
+            new User { Name = "Bob", Age = 30 },
+            new User { Name = "Charlie", Age = 35 }
+            };
+
+            List<User> filteredUsers = FilterUsers(users, u => u.Age > 25);
+
+            foreach (var user in filteredUsers)
+            {
+                Console.WriteLine($"{user.Name} - {user.Age}");
+            }
+        }
+        static List<User> FilterUsers(List<User> users, Func<User, bool> filterCriteria)
+        {
+            List<User> filteredUsers = new List<User>();
+            foreach (var user in users)
+            {
+                if (filterCriteria(user))
+                {
+                    filteredUsers.Add(user);
+                }
+            }
+            return filteredUsers;
+        }
+    }
+}
