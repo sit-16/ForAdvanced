6dccd4d71ec61644e5c43623d2b748a722628a81
Vovan <vhotsanovich@yandex.ru>
2026-04-10 23:42:58 +0800

    17.3 События.08.Регистрация пользователя 1

---
+++ 17.3 События.08.Регистрация пользователя 1/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _17._3_События._08.Регистрация_пользователя_1
+{
+    public class User
+    {
+        public string Name;
+        public string Email;
+        public string Password;
+
+        public User(string name, string email, string password)
+        {
+            Name = name;
+            Email = email;
+            Password = password;
+        }
+    }
+
+    public class UserManager
+    {
+        public List<User> Users { get; }
+        public UserManager()
+        {
+            Users = new List<User>();
+        }
+        public void  Register(User user)
+        { 
+            this.Users.Add(user);
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UserManager userManager = new UserManager();
+            userManager.Register(new User("Алексей", "a.potapov@gmail.com", "qwerty123"));
+            Console.WriteLine($"Пользователь {userManager.Users[0].Name} зарегистрирован");
+        }
+    }
+}
