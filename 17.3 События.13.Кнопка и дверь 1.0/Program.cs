b8b2a3e36481c89ee33386f1369bd8303f87b7c5
Vovan <vhotsanovich@yandex.ru>
2026-04-15 22:02:45 +0800

    17.3 События.14.Кнопка и дверь 2.0

---
+++ 17.3 События.13.Кнопка и дверь 1.0/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _17._3_События._13.Кнопка_и_дверь_1._0
+{
+    public class Button
+    {
+        private string identity;
+
+        public Button(string identity)
+        {
+            this.identity = identity;
+        }
+    }
+
+    public class ButtonClickedEventArgs : EventArgs
+    { 
+        public string Location { get; }
+        public int Number { get; }
+        public ButtonClickedEventArgs(string arg)
+        {
+            string[] args = arg.Split('#');
+            this.Location = args[0];
+            this.Number = int.Parse(args[1]);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ButtonClickedEventArgs buttonClickedEventArgs = new ButtonClickedEventArgs("Подвал#4");
+            Console.WriteLine($"Название комнаты: {buttonClickedEventArgs.Location}");
+            Console.WriteLine($"Номер кнопки: {buttonClickedEventArgs.Number}");
+        }
+    }
+}
---
+++ 17.3 События.14.Кнопка и дверь 2.0/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _17._3_События._13.Кнопка_и_дверь_2._0
+{
+    public class Button
+    {
+        private string identity;
+        public event EventHandler<ButtonClickedEventArgs> ClickHandler;
+
+        public Button(string identity)
+        {
+            this.identity = identity;
+        }
+        public void Clicked()
+        {
+            var eventArgs = new ButtonClickedEventArgs(this.identity);
+            this.ClickHandler?.Invoke(this, eventArgs);
+        }
+    }
+
+    public class ButtonClickedEventArgs : EventArgs
+    {
+        public string Location { get; }
+        public int Number { get; }
+        public ButtonClickedEventArgs(string identity)
+        {
+            string[] args = identity.Split('#');
+            this.Location = args[0];
+            this.Number = int.Parse(args[1]);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Button button = new Button("Столовая#4");
+            button.ClickHandler += (object sender, ButtonClickedEventArgs e) =>
+            {
+                Console.WriteLine($"Нажата кнопка в локации: {e.Location}, с номером {e.Number}");
+            };
+            button.Clicked();
+        }
+    }
+}
