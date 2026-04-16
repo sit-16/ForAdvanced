497188e04cc0f3a2498f5c17d833a879cce5363e
Vovan <vhotsanovich@yandex.ru>
2026-04-16 23:49:16 +0800

    17.3 События.14.Кнопка и дверь 3.0

---
+++ 17.3 События.14.Кнопка и дверь 3.0/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace _17._3_События._13.Кнопка_и_дверь_3._0
+{
+    public class Door
+    {
+        private void Open(object sender, ButtonClickedEventArgs eventArgs)
+        {
+            Console.WriteLine($"Дверь открыта кнопкой с номером {eventArgs.Number} в комнате {eventArgs.Location}");
+        }
+        public void Init(Button button) 
+        {
+            button.ClickHandler += Open;
+        }
+    }
+    
+    public class Button
+    {
+        private string identity;
+        public event EventHandler<ButtonClickedEventArgs> ClickHandler;
+
+        public Button(string identity)
+        {
+            this.identity = identity;
+        }
+        public void Clicked()
+        {
+            var eventArgs = new ButtonClickedEventArgs(this.identity);
+            this.ClickHandler?.Invoke(this, eventArgs);
+        }
+    }
+
+    public class ButtonClickedEventArgs : EventArgs
+    {
+        public string Location { get; }
+        public int Number { get; }
+        public ButtonClickedEventArgs(string identity)
+        {
+            string[] args = identity.Split('#');
+            this.Location = args[0];
+            this.Number = int.Parse(args[1]);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Button button = new Button("Столовая#4");
+            Door doorA = new Door();
+            Door doorB = new Door();
+            doorA.Init(button);
+            doorB.Init(button);
+            button.Clicked();
+        }
+    }
+}
