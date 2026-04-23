0f5b4e26e5f0b261a3cabb002c30887f834269d4
Vovan <vhotsanovich@yandex.ru>
2026-04-23 23:28:08 +0800

    18.2 Иерархичное и множественное наследование интерфейсов.8.От класса и интерфейсов

---
+++ 18.2 Иерархичное и множественное наследование интерфейсов.8.От класса и интерфейсов/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _18._2_Иерархичное_и_множественное_наследование_интерфейсов._8.От_класса_и_интерфейсов
+{
+    public interface IStudent
+    {
+        int Year { get; set; }
+        void DoHomework();
+    }
+
+    public interface IWorker
+    {
+        // Зарплата
+        int Salary { get; set; }
+        void Work();
+    }
+
+    public class Person
+    {
+        string Name { get; set; }
+        int Age { get; set; }
+    }
+
+    public class Student: Person, IStudent, IWorker
+    {
+        public int Year { get; set; }
+        public int Salary { get; set; }
+        public void Work()
+        { }
+        public void DoHomework()
+        { }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
